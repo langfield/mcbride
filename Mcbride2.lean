c5331e424b457fcd4ece163dbf33d5f7ff3c3d6a
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-06-23 18:03:21 -0400

    Get stuck on identity from combinators.

--- Mcbride2.lean
+++ Mcbride2.lean
@@ -10,7 +10,11 @@ def mapEither {α α' β β' : Type u} (f : α → α') (g : β → β') : Eithe
 | Either.inr b => Either.inr <| g b
 
 -- 'K' for 'KONSTANT' (in German).
-def combinatorK {α ε : Type u} : α → ε → α := λ a e => a
+def combinatorK {α ε : Type u} : α → (ε → α) := λ a e => a
 
-def combinatorS {α b γ : Type u} : (α → β → γ) → (γ → α) → γ → β :=
-  λ a b c => sorry
+def combinatorS {α b γ : Type u} : (γ → α → β) → (γ → α) → γ → β :=
+  λ abc ca c => abc c (ca c)
+
+-- `λ x => x` is the easy way; let's do it a funny way to make a point.
+def identity {χ : Type u} : χ → χ :=
+  combinatorS combinatorK (@combinatorK χ Zero)
