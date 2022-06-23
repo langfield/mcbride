f5cdd9c192725426d36b243bc5a9c79f91864722
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2022-06-23 17:10:17 -0400

    Add `mapEither()` function.

--- Mcbride2.lean
+++ Mcbride2.lean
@@ -4,3 +4,7 @@ universe u
 
 def mapPair {α α' β β' : Type u} (f : α → α') (g : β → β') : Product α β → Product α' β'
 | ⟨a, b⟩ => ⟨f a, g b⟩
+
+def mapEither {α α' β β' : Type u} (f : α → α') (g : β → β') : Either α β → Either α' β'
+| Either.inl a => Either.inl <| f a
+| Either.inr b => Either.inr <| g b
